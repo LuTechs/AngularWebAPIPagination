af135dfc5ffcdaa2ace03e607df03f7a3b5343e5
LENG UNG <ungleng@gmail.com>
2016-02-10 09:53:01 +1100

    update frontend library and add readme

--- AngularWebAPIPagination/Angular/ListCtrl.js
+++ AngularWebAPIPagination/Angular/ListCtrl.js
@@ -1,13 +1,16 @@
 ﻿var listModule = angular.module('ListProduct', ['ui.bootstrap', 'services.rest']);
 
 listModule.controller('ListProductCtrl', [
-    '$scope', 'productFactory', function ($scope, productFactory) {
+    '$scope', 'productFactory','$window','$location', function ($scope, productFactory,$window,$location) {
 
         $scope.products = [];
         $scope.pagingOptions = { pageSize: 18, currentPage: 1 };
         $scope.totalItems = 0;
         $scope.numPages = 0;
 
+        $window.addEventListener("popstate", function (e) {
+       
+        });
         productFactory.getProductsByPage($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage)
             .success(function (data) {
                 $scope.products = data.products;
@@ -22,9 +25,9 @@ listModule.controller('ListProductCtrl', [
 
             productFactory.getProductsByPage($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage)
             .success(function (data) {
-              $scope.products = data.products;
-              $scope.totalItems = data.totalItems;
-                })
+                $scope.products = data.products;
+                $scope.totalItems = data.totalItems;
+            })
           .error(function (err) {
               console.log(err);
           });
---
+++ README.md
@@ -0,0 +1,3 @@
+# SimpleAjaxPagedListAndSearchMVC5
+This MVC and WebAPI application will demonstrate how to create pagedlist with AngularJS
+![](https://raw.github.com/ungleng/AngularWebAPIPagination/master/screenshot/screenshot.png)
